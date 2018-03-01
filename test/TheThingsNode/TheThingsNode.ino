1f4003880472adc59b1e82dde7660204eb1d48c0
Charles <hallard04@free.fr>
2018-03-01 13:50:20 +0100

    fixed wake with wakeReason parameter

--- test/TheThingsNode/TheThingsNode.ino
+++ test/TheThingsNode/TheThingsNode.ino
@@ -4,7 +4,7 @@ TheThingsNode *node;
 
 void setup() {
     node = TheThingsNode::setup();
-    node->onWake(voidFn);
+    node->onWake(wakeFn);
     node->onSleep(voidFn);
     node->onInterval(onInterval);
     node->configInterval(false, 60000);
@@ -37,6 +37,7 @@ void setup() {
 }
 
 void voidFn() {}
+void wakeFn(uint8_t wakeStatus) {}
 
 void durationFn(unsigned long duration) {}
 
